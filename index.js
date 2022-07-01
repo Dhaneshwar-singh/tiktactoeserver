0424c26ff21bced2afb17ba1c1c2475883df22e7
Dhaneshwar Singh <75466952+Dhaneshwar-singh@users.noreply.github.com>
2022-07-01 13:13:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var io = require("socket.io")(server);
 app.use(express.json());
 
 const DB =
-  "mongodb+srv://rivaan:test123@cluster0.rmhtu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://sheeba:sheebabot1@sheebabot.mwov3.mongodb.net/sheeba?retryWrites=true&w=majority";
 
 io.on("connection", (socket) => {
   console.log("connected!");
